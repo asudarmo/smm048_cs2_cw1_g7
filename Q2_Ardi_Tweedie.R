039fc4d83d8014036e459d67b28787b73d172c7b
asudarmo <44960986+asudarmo@users.noreply.github.com>
2026-03-05 22:12:59 +0000

    Experiment with Tweedie to model claim severity in Q2_Ardi_Tweedie.R

---
+++ Q2_Ardi_Tweedie.R
@@ -0,0 +1,49 @@
+library(insuranceData)
+
+data(dataCar)
+df2 <- data.frame(dataCar)
+
+library(GJRM)
+
+
+# Can fit P for the claim frequency just fine
+P.freq <- GJRM::gamlss(list(numclaims ~ 1), family = "P", data = df2, offset = log(exposure))
+
+res.check(P.freq)
+summary(P.freq)
+AIC(P.freq)
+# 34943.67
+
+# Requires loading mgcv for ldTweedie to fit Tweedie below
+library(mgcv)
+# Now can fit Tweedie for the claim amount just fine
+TW.sev <- GJRM::gamlss(list(claimcst0 ~ s(exposure) + area + gender + agecat, # mu
+                               ~ s(veh_value) + agecat + s(exposure), #sigma
+                               ~ gender + agecat + s(exposure)),  # nu
+                          family = "TW", data = df2)
+
+
+
+res.check(TW.sev)
+summary(TW.sev)
+AIC(TW.sev)
+# 111391.2
+
+
+# Encountered ERROR here
+P.TW.C0 <- GJRM::gjrm(list(numclaims ~ 1, # mu1 for freq
+                           claimcst0 ~ s(exposure) + area + gender + agecat, # mu2 for sev
+                           ~ s(veh_value) + agecat + s(exposure), #sigma2
+                           ~ gender + agecat + s(exposure), #nu2
+                           ~ 1 # theta for the copula
+                      ),
+                      data = df2, 
+                      margins = c("P", "TW"),
+                      offset1=log(exposure),
+                      model = "B",
+                      copula = "C0")
+
+# Error in names(log.nu.2) <- "nu.2.star" : 
+#   attempt to set an attribute on NULL
+
+
