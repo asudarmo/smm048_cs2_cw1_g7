bf71774d08c979aaf4682640744aa248120338dd
asudarmo <44960986+asudarmo@users.noreply.github.com>
2026-03-06 09:56:54 +0000

    Update Q2_Ardi_Tweedie.R with model on truncated data.

--- Q2_Ardi_Tweedie.R
+++ Q2_Ardi_Tweedie.R
@@ -33,9 +33,9 @@ AIC(TW.sev)
 # Encountered ERROR here
 P.TW.C0 <- GJRM::gjrm(list(numclaims ~ 1, # mu1 for freq
                            claimcst0 ~ s(exposure) + area + gender + agecat, # mu2 for sev
-                           ~ s(veh_value) + agecat + s(exposure), #sigma2
-                           ~ gender + agecat + s(exposure), #nu2
-                           ~ 1 # theta for the copula
+                           ~ s(veh_value) + agecat + s(exposure), #sigma2 ?
+                           ~ gender + agecat + s(exposure), #nu2 ?
+                           ~ 1 # theta for the copula ?
                       ),
                       data = df2, 
                       margins = c("P", "TW"),
@@ -46,4 +46,29 @@ P.TW.C0 <- GJRM::gjrm(list(numclaims ~ 1, # mu1 for freq
 # Error in names(log.nu.2) <- "nu.2.star" : 
 #   attempt to set an attribute on NULL
 
+
+
+######
+# Model on Truncated data: strictly positve claims amount
+
+df2.trunc <- df2[df2$claimcst0 > 0,]
+
+# This works:
+tP.IG.C0 <- gjrm(list(numclaims ~ 1,
+                     claimcst0 ~ 1,
+                     ~ 1,
+                     ~ 1
+                    ),
+                data = df2.trunc, 
+                margin = c("tP", "IG"),
+                offset1=log(exposure),
+                model = "B",
+                copula = "C0")
+
+res.check(tP.IG.C0)
+
+summary(tP.IG.C0)
+
+AIC(tP.IG.C0)
+# 79321.95
 
