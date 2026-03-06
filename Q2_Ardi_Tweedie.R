a388d8cbca9219c5efc9216aa96d3c7532b76fa9
asudarmo <44960986+asudarmo@users.noreply.github.com>
2026-03-06 10:42:14 +0000

    Add baseline Tweedie model for comparison.

--- Q2_Ardi_Tweedie.R
+++ Q2_Ardi_Tweedie.R
@@ -5,8 +5,9 @@ df2 <- data.frame(dataCar)
 
 library(GJRM)
 
+## Fitting the marginal distributions
 
-# Can fit P for the claim frequency just fine
+# Fit Poisson for the claim frequency 
 P.freq <- GJRM::gamlss(list(numclaims ~ 1), family = "P", data = df2, offset = log(exposure))
 
 res.check(P.freq)
@@ -17,16 +18,27 @@ AIC(P.freq)
 # Requires loading mgcv for ldTweedie to fit Tweedie below
 library(mgcv)
 # Now can fit Tweedie for the claim amount just fine
-TW.sev <- GJRM::gamlss(list(claimcst0 ~ s(exposure) + area + gender + agecat, # mu
+
+TW.sev1 <- GJRM::gamlss(list(claimcst0 ~ 1, # mu
+                               ~ 1, #sigma
+                               ~ 1),  # nu
+                          family = "TW", data = df2)
+
+
+res.check(TW.sev1)
+summary(TW.sev1)
+AIC(TW.sev1)
+# 113114.1
+
+TW.sev2 <- GJRM::gamlss(list(claimcst0 ~ s(exposure) + area + gender + agecat, # mu
                                ~ s(veh_value) + agecat + s(exposure), #sigma
                                ~ gender + agecat + s(exposure)),  # nu
                           family = "TW", data = df2)
 
 
-
-res.check(TW.sev)
-summary(TW.sev)
-AIC(TW.sev)
+res.check(TW.sev2)
+summary(TW.sev2)
+AIC(TW.sev2)
 # 111391.2
 
 
@@ -53,22 +65,20 @@ P.TW.C0 <- GJRM::gjrm(list(numclaims ~ 1, # mu1 for freq
 
 df2.trunc <- df2[df2$claimcst0 > 0,]
 
-# This works:
+# This works just fine:
 tP.IG.C0 <- gjrm(list(numclaims ~ 1,
                      claimcst0 ~ 1,
                      ~ 1,
                      ~ 1
                     ),
                 data = df2.trunc, 
-                margin = c("tP", "IG"),
+                margins = c("tP", "IG"),
                 offset1=log(exposure),
                 model = "B",
                 copula = "C0")
 
 res.check(tP.IG.C0)
-
 summary(tP.IG.C0)
-
 AIC(tP.IG.C0)
 # 79321.95
 
